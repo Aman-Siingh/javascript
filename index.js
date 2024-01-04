4f2497c6c77fb2f6ca4f0b5c9c513977cbae60ff
Aman-Siingh <amansiingh15@gmail.com>
2024-01-04 22:40:17 +0530

    Variables

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ console.log("hello Website")
 
 // window.alert("Confirm submission")
 
-document.getElementById("myH1").textContent = "Aman Singh";
-document.getElementById("myP").textContent = "Passionate Software developer";
+// document.getElementById("myH1").textContent = "Aman Singh";
+// document.getElementById("myP").textContent = "Passionate Software developer";
+
+let Name="Aman Singh";
+let Age= 18;
+let isStudent="True";
+
+document.getElementById("p1").textContent=`My name is ${Name}`;
+document.getElementById("p2").textContent=`I am ${Age} year old `;
+document.getElementById("p3").textContent=`Enrolled :${isStudent}`;
 
