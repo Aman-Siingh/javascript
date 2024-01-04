6f8c96665f783c5c136a0a412a5e623484ef3b62
Aman-Siingh <amansiingh15@gmail.com>
2024-01-04 09:22:15 +0530

    Basics

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("hello Website")
+
+// window.alert("Confirm submission")
+
+document.getElementById("myH1").textContent = "Aman Singh";
+document.getElementById("myP").textContent = "Passionate Software developer";
+
