b27328a1c4913906b701ef51bf764c8b5c91fe7e
Pooja Singh <amansiingh15@gmail.com>
2024-01-18 21:10:50 +0530

    docs : add arrays

---
+++ ch25-arrays/index.js
@@ -0,0 +1,30 @@
+// array - a data structure consisting of a collection of elements (values or variables),
+//         of same memory size, each identified by at least one array index or key
+
+let fruits=["apple", "mango","orange"];
+
+// fruits.pop();
+// fruits.unshift("mango");
+// fruits.shift();
+// fruits.push("cocnuts");
+
+
+// let length=fruits.length;
+// console.log(length);
+
+// let index = fruits.indexOf("mango");
+// console.log(index);
+
+
+// console.log(fruits)
+
+fruits.sort().reverse();
+
+for(let i of fruits){
+    console.log(i);
+}
+
+
+
+
+
