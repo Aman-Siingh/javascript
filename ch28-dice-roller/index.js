917ce7b37792d5ae8594f4c25ee2a4ee56449754
Pooja Singh <amansiingh15@gmail.com>
2024-01-20 23:41:32 +0530

    feat :

---
+++ ch28-dice-roller/index.js
@@ -0,0 +1,28 @@
+
+function rollDice(){
+
+    const numofDice = document.getElementById("numOfDice").value;
+    let diceResult = document.getElementById("diceResult");
+    let diceImage = document.getElementById("diceImage");
+
+    let image = [];
+    let value = [];
+
+
+
+    for (let i=1; i<=numofDice ;i++){
+
+        let num = Math.floor(Math.random()*6)+1
+
+        value.push(num);
+
+        image.push(`<img src="dice/${num}.svg">`);
+
+       
+
+    }
+
+    diceResult.textContent=value.join("-")
+    diceImage.innerHTML=image.join("  ")
+}
+
