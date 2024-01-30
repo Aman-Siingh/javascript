0a524cfe89f3fe285c79ec415942af3c21b56826
Pooja Singh <amansiingh15@gmail.com>
2024-01-30 22:30:51 +0530

    feat : map method

--- ch32-map/index.js
+++ ch32-map/index.js
@@ -1,1 +1,26 @@
-// map ---- 
+// map ---- it ittrate over each elemnt of an Array a applies a function over each element 
+// iit return a new array 
+
+
+function toUpper(element){
+
+    return element.toUpperCase();
+}
+
+let A = ["mango", "apple ", "banana", "orange"];
+
+const B = A.map(toUpper);
+
+console.log(A);
+console.log(B);
+
+
+const dates = ["2002-09-01", "2005-07-03",'2024-06-30'];
+
+function format(element){
+     const parts = element.split("-");
+     console.log(`${parts[2]}/${parts[1]}/${parts[0]}`);
+
+}
+
+dates.map(format);
