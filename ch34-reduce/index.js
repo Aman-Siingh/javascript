f2b16a30279551137dc45f31637d55aae85bc0c3
Pooja Singh <amansiingh15@gmail.com>
2024-02-05 22:09:07 +0530

    feat : update  index.js

--- ch34-reduce/index.js
+++ ch34-reduce/index.js
@@ -11,3 +11,18 @@ function sum(accumiilator , element){
 }
 
 console.log(total);
+
+const grades = [75, 50, 90, 80, 65, 95];
+
+const maximum = grades.reduce (getMax);
+const minimum = grades.reduce (getMin);
+
+console.log (maximum);
+console.log(minimum);
+
+function getMax (accumulator, element){
+    return Math.max (accumulator, element);
+}
+function getMin (accumulator, element){
+    return Math.min(accumulator, element);
+}
