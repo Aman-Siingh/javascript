826da25db678a4309ff9799b6592e7fa04098abf
Pooja Singh <amansiingh15@gmail.com>
2024-03-03 23:23:48 +0530

    feat : update inheritance

--- ch42-inheritance/index.js
+++ ch42-inheritance/index.js
@@ -1,18 +1,28 @@
-class Animal {...
-}
-    class Rabbit extends Animal{
-    name = "rabbit";
-}
-class Fish extends Animal{
-    name = "fish";
-}
-class Hawk extends Animal {
-    name = "hawk";
+class Animal{
+
+    alive = true ;
+
+    eat(){
+        console.log(`this ${this.name} is Eating`);
+    }
+
+    sleeping(){
+        console.log(`this ${this.name} is sleeping `);
+    }
 }
 
+class rabbit extends Animal{
+    name = "Rabbit ";
+}
 
+class fish extends Animal{
+    name="Fish"
+}
 
-        const rabbit new Rabbit ();
-        const fish new Fish ();
-        const hawk new Hawk();
-        console.log(rabbit.alive);
+console.log(new fish().alive);
+new fish().eat();
+new fish().sleeping();
+
+console.log(new rabbit().alive);
+new rabbit().eat();
+new rabbit().sleeping();
